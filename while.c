dccff9150de915c22e29478bb73c2744f1c8efd3
Hacker-rizwan <167663866+Hacker-rizwan@users.noreply.github.com>
2024-12-28 15:43:49 +0530

    Create while.c
    simple code

---
+++ while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+    printf("Enter the value of i: ");
+    scanf("%d", &i);
+    while (i > 0) {
+        printf("He is an innocent boy\n");
+        i--; 
+    }
+}
